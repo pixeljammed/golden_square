e8ab3395752ed25ed95d9ad7a2f888dbf56a64d1
Maakis <tmaakis@gmail.com>
2025-10-07 17:15:38 +0100

    finish up the car system

--- pair/test_drive_class_system/lib/tyre_system.py
+++ pair/test_drive_class_system/lib/tyre_system.py
@@ -6,14 +6,21 @@ class Car:
         self.TyreRR = TyreRR
 
     def view_tyres(self):
+        tyres = [self.TyreFL.view_tyre()[-1].copy(), self.TyreFR.view_tyre()
+                 [-1].copy(), self.TyreRL.view_tyre()[-1].copy(), self.TyreRR.view_tyre()[-1].copy()]
+        tyres[0]["position"] = "FL"
+        tyres[1]["position"] = "FR"
+        tyres[2]["position"] = "RL"
+        tyres[3]["position"] = "RR"
+        return tyres
 
 
 class Tyre:
     def __init__(self, initial_record):
-        pass
+        self.history = [initial_record]
 
     def view_tyre(self):
-        pass
+        return self.history
 
-    def record(record):
-        pass
+    def record(self, record):
+        self.history.append(record)
--- pair/test_drive_class_system/tests/test_tyre_system.py
+++ pair/test_drive_class_system/tests/test_tyre_system.py
@@ -1,32 +1,45 @@
 import pytest
 from lib.tyre_system import *
 
-def test_tyre_system():
-    front_left_tyre = Tyre({"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5})
-    front_right_tyre = Tyre({"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5})
-    rear_left_tyre = Tyre({"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5})
-    rear_right_tyre = Tyre({"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5})
 
-    my_car = Car(front_left_tyre, front_right_tyre, rear_left_tyre, rear_right_tyre)
+def test_tyre_system():
+    front_left_tyre = Tyre(
+        {"date": "28-07-2007", "pressure": 24, "tread": 1.5})
+    front_right_tyre = Tyre(
+        {"date": "28-07-2007", "pressure": 24, "tread": 1.5})
+    rear_left_tyre = Tyre({"date": "28-07-2007", "pressure": 24, "tread": 1.5})
+    rear_right_tyre = Tyre(
+        {"date": "28-07-2007", "pressure": 24, "tread": 1.5})
+
+    my_car = Car(front_left_tyre, front_right_tyre,
+                 rear_left_tyre, rear_right_tyre)
 
     assert my_car.view_tyres() == [
-        {"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5, "position" : "FL"},
-        {"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5, "position" : "FR"},
-        {"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5, "position" : "RL"},
-        {"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5, "position" : "RR"}
+        {"date": "28-07-2007", "position": "FL", "pressure": 24, "tread": 1.5},
+        {"date": "28-07-2007", "position": "FR", "pressure": 24, "tread": 1.5},
+        {"date": "28-07-2007", "position": "RL", "pressure": 24, "tread": 1.5},
+        {"date": "28-07-2007", "position": "RR", "pressure": 24, "tread": 1.5}
     ]
 
-    assert my_car.TyreFL.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}]
-    assert my_car.TyreFR.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}]
-    assert my_car.TyreRL.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}]
-    assert my_car.TyreRR.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}]
+    assert my_car.TyreFL.view_tyre() == [
+        {"date": "28-07-2007", "pressure": 24, "tread": 1.5}]
+    assert my_car.TyreFR.view_tyre() == [
+        {"date": "28-07-2007", "pressure": 24, "tread": 1.5}]
+    assert my_car.TyreRL.view_tyre() == [
+        {"date": "28-07-2007", "pressure": 24, "tread": 1.5}]
+    assert my_car.TyreRR.view_tyre() == [
+        {"date": "28-07-2007", "pressure": 24, "tread": 1.5}]
 
-    my_car.TyreRL.record({"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1})
-    my_car.TyreRL.record({"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1})
-    my_car.TyreRL.record({"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1})
-    my_car.TyreRL.record({"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1})
+    my_car.TyreFL.record({"date": "15-03-2014", "pressure": 3, "tread": 0.1})
+    my_car.TyreFR.record({"date": "15-03-2014", "pressure": 3, "tread": 0.1})
+    my_car.TyreRL.record({"date": "15-03-2014", "pressure": 3, "tread": 0.1})
+    my_car.TyreRR.record({"date": "15-03-2014", "pressure": 3, "tread": 0.1})
 
-    assert my_car.TyreFL.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}, {"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1}]
-    assert my_car.TyreFR.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}, {"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1}]
-    assert my_car.TyreRL.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}, {"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1}]
-    assert my_car.TyreRR.view_tyre() == [{"date" : "28-07-2007", "pressure" : 24, "tread" : 1.5}, {"date" : "15-03-2014", "pressure" : 3, "tread" : 0.1}]
+    assert my_car.TyreFL.view_tyre() == [{"date": "28-07-2007", "pressure": 24, "tread": 1.5}, {
+        "date": "15-03-2014", "pressure": 3, "tread": 0.1}]
+    assert my_car.TyreFR.view_tyre() == [{"date": "28-07-2007", "pressure": 24, "tread": 1.5}, {
+        "date": "15-03-2014", "pressure": 3, "tread": 0.1}]
+    assert my_car.TyreRL.view_tyre() == [{"date": "28-07-2007", "pressure": 24, "tread": 1.5}, {
+        "date": "15-03-2014", "pressure": 3, "tread": 0.1}]
+    assert my_car.TyreRR.view_tyre() == [{"date": "28-07-2007", "pressure": 24, "tread": 1.5}, {
+        "date": "15-03-2014", "pressure": 3, "tread": 0.1}]
